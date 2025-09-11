2f40707943a209cc2c10a3194600d16f903fb468
DESKTOP-HQRGA2D\Bala <balasaravanan.v@v4csolutions.com>
2025-09-11 15:54:29 +0530

    login page created

---
+++ app/Controllers/admin/Login.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controllers\admin;
+use App\Controllers\BaseController;
+
+class Login extends BaseController 
+{
+	  
+	 public function index(): string
+    {
+         return view('admin/login');
+          
+       
+    }
+}
