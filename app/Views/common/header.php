e716a2e5b4ae0807e3e9ccd01248ffe61af86354
DESKTOP-HQRGA2D\Bala <balasaravanan.v@v4csolutions.com>
2025-09-13 16:50:00 +0530

    website integration complete

---
+++ app/Views/common/header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>CEED - Empowering Kids with Growth</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<link rel="stylesheet" href="<?php echo base_url().ASSET_PATH; ?>assets/css/styles.css">
+		<link rel="stylesheet" href="<?php echo base_url().ASSET_PATH; ?>assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="<?php echo base_url().ASSET_PATH; ?>assets/css/bootstrap-icons.min.css">
+	</head>
+	<body>
+		<header>
+			<div class="container-lg">
+				<div class="col-md-12">
+					<div class="main-menu-box">
+						<div class="menu-lg">
+							<div class="row">
+								<div class="col-1 logo">
+									<img src="<?php echo base_url().ASSET_PATH; ?>assets/img/logo.png" />
+								</div>
+								<div class="col-7"></div>
+								<div class="col-4 text-right btn-holder">
+									<button class="btn btn-login">login</button>
+									<button class="btn btn-demo">Request A Demo</button>
+								</div>
+							</div>	
+						</div>
+						<div class="menu-resp">
+							<div class="row">
+								<div class="col-3 logo">
+									<img src="<?php echo base_url().ASSET_PATH; ?>assets/img/logo.png" />
+								</div>
+								<div class="col-2 text-right">
+									<a href="#">Login</a>
+								</div>
+								<div class="col-7 text-right">
+									<button class="btn btn-demo">Request A Demo</button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<div class="row">
+							<div class="col-md-12 text-center head-title">
+								<h1><span class="orange">Empowering Kids with<br/>Growth Mindset Through<span><br/><span class="palesky">the Power of games.</span></h1>
+							</div>
+							<div class="col-md-12 text-center">
+								<div class="clearfix">&nbsp;</div>
+								<p class="palesky">Designed for busy families, CEED turns your child’s screen<br/>time into growth time with daily micro-lessons and brain-<br/>building games.</p>
+							</div>
+							<div class="col-md-12">
+								<div class="puzzle">
+									<img src="<?php echo base_url().ASSET_PATH; ?>assets/img/puzzle.png" />
+									<div class="jix-mideset"></div>
+									<div class="jix-play"></div>
+									<div class="jix-learning"></div>
+									<div class="jix-thinking"></div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
