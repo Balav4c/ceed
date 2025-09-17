52f59fc672ec6dedf852343cb0936e5fca5f683d
shafeena <shafeenaki123@gmail.com>
2025-09-17 15:04:50 +0530

    route ,dashboard,login,

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,7 +6,11 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+// $routes->get('admin/dashboard', 'admin\Dashboard::index');
 $routes->get('admin/dashboard', 'admin\Dashboard::index');
+$routes->get('user/dashboard', 'admin\UserDashboard::index');
+
+
 $routes->get('admin', 'admin\Login::index');
 $routes->post('admin/login', 'admin\Login::login');
 $routes->get('admin/logout', 'admin\Login::logout');
