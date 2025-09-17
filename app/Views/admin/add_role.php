07a8b449bcc451b2ae4734e9347a299bd894ab03
shafeena <shafeenaki123@gmail.com>
2025-09-17 15:40:47 +0530

    addrole

--- app/Views/admin/add_role.php
+++ app/Views/admin/add_role.php
@@ -24,7 +24,7 @@
                             </div>
                             <div class="row">
                                 <?php
-                                    $menus = ['Dashboard', 'Manage User', 'Manage Role'];
+                                    $menus = ['Dashboard', 'Manage Role','Manage Admin User','Manage Course' ];
                                     foreach ($menus as $menu) : 
                                         $menuKey = ucwords(str_replace('_', ' ', $menu));
                                             $isChecked = (isset($access[$menuKey]) && $access[$menuKey] == 1) ? 'checked' : '';
